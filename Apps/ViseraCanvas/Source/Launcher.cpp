55ec8d73189efa15ada0f23f31a66179d7111ed1
LJYC <ljyc.me@gmail.com>
2025-01-06 13:16:07 +0900

    Update

--- Apps/ViseraCanvas/Source/Launcher.cpp
+++
@@ -1,9 +0,0 @@
-import Visera;
-import ViseraCanvas;
-using namespace VISERA_APP_NAMESPACE;
-
-int main(int argc, char* argv[])
-{	
-	VE::Visera ViseraEngine( new App );
-	return ViseraEngine.Run();
-}
